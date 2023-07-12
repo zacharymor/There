6760a0940362f866334fa38df7b39800abd9608b
Derek Mor <derekindirections@gmail.com>
2023-07-12 13:31:09 -0400

    Initial Commit

---
+++ There/ThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThereApp.swift
+//  There
+//
+//  Created by Zachary Mor on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
